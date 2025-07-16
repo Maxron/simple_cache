c680d6bd19664fd20aeba81e533282f2e14af205
Wei <zero.maxima@gmail.com>
2025-07-17 01:29:00 +0800

    feat: add simple cache implementation

--- lib/simple_cache.dart
+++ lib/simple_cache.dart
@@ -1,14 +1,8 @@
-// You have generated a new plugin project without specifying the `--platforms`
-// flag. A plugin project with no platform support was generated. To add a
-// platform, run `flutter create -t plugin --platforms <platforms> .` under the
-// same directory. You can also find a detailed instruction on how to add
-// platforms in the `pubspec.yaml` at
-// https://flutter.dev/to/pubspec-plugin-platforms.
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
 
-import 'simple_cache_platform_interface.dart';
-
-class SimpleCache {
-  Future<String?> getPlatformVersion() {
-    return SimpleCachePlatform.instance.getPlatformVersion();
-  }
-}
+export 'src/simple_cache_base.dart';
+export 'src/in_memory_cache.dart';
+export 'src/shared_preference_cache.dart';
